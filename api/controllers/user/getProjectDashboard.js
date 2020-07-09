bdadfa3fccde2e8b223257beaa7f1c6d1fd02ab8
Vatsal Kandoi <vatsalkandoi1998@gmail.com>
2020-07-09 17:54:12 +0530

    Added fetching project dashboard

--- api/controllers/user/getProjectDashboard.js
+++ api/controllers/user/getProjectDashboard.js
@@ -1,4 +1,4 @@
-const {User} = require('../../models');
+const {User, Project} = require('../../models');
 const logger = require('../../../config/winston');
 const {ServerError, Success, AuthError} = require('../../responses');
 const project = require('../../models/project');
@@ -14,6 +14,11 @@ module.exports = async(req, res) => {
         const hasAccess = await Project.findOne({_id: projectId, 'users.user': userId});
         if (hasAccess == null) return res.json(AuthError);
 
+        let access;
+        hasAccess.users.forEach(element => {
+            if (element.user._id == userId) { access = element.access; }
+        });
+
         const project = await Project.findOne({_id: projectId}).populate({path: 'users.user', select: 'name email'})
             .populate({path: 'closed.admin.by', select: 'name email'})
             .populate({path: 'closed.client.by', select: 'name email'})
@@ -21,10 +26,11 @@ module.exports = async(req, res) => {
                 populate: [{path: 'completed.by', select: 'name email'}, {path: 'accepted.by', select: 'name email'}] })
             .populate({path: 'issues', select: 'description addedBy addedOn closed',
                 populate: [{path: 'addedBy', select: 'name email'}, {path: 'closed.by', select: 'name email'}]})
-            .select('users features issues closed links')
+            .select('users features issues closed links name')
             .lean();
-        return res.json({...Success, ...project});
+        return res.json({...Success, ...project, access});
     } catch(err) {
+        console.log(err);
         logger.error({error: err, message: "An error occured"});
         return res.json(ServerError);    
     }
